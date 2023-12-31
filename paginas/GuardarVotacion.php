55925b34cfb37b80e0ea9eb853e1fe1b70a36698
Deniel Rodriguez S <dan.avalon@gmail.com>
2023-12-31 16:01:40 -0300

    Add files via upload

--- paginas/GuardarVotacion.php
+++ paginas/GuardarVotacion.php
@@ -15,34 +15,44 @@ $lista = array($opcion1, $opcion2, $opcion3, $opcion4);
 // Concatenar los datos con coma como separador
 $comoseentero = implode(",", $lista);
 
-// obtiene el nombre de la region
-$sqle = "SELECT NOMBRE_REGION FROM regiones WHERE ID_REGION = '$idregion'";
-$resp = $link->query($sqle);
-while ($row = mysqli_fetch_array($resp) ) {
-   $region = $row['NOMBRE_REGION']; 
-}
 
-$array = array( 
-				"nombre"      => limpiar_signos($_POST['nomapellido']),
-				"alias"       => limpiar_signos($_POST['alias']),
-                "rut"  		  => $_POST['rut'],
-                "email"       => limpiar_signos($_POST['email']),
-                "region"      => $region,
-                "comuna"      => $_POST['comunas'],
-                "candidato"   => $_POST['candidatos'],
-                "comoseentero" => $comoseentero,
-                "fecha"        => date("Y-m-d H:i:s"));
+// validacion de duplicacion de voto
+$sqlr = "SELECT * FROM votaciones WHERE rut = '".$_POST['rut']."'";
+$resp1 = $link->query($sqlr);
+if ($resp1->num_rows == 0) {
 
-// almacena datos
-$sql  = "INSERT INTO votaciones";
-$sql .= " (`".implode("`, `", array_keys($array))."`)";
-$sql .= " VALUES ('".implode("', '", $array)."') ";
-$result = $link->query($sql) or die(mysqli_error($link));
-// Cerrar la conexión
-$link->close();
+   // obtiene el nombre de la region
+   $sqle = "SELECT NOMBRE_REGION FROM regiones WHERE ID_REGION = '$idregion'";
+   $resp = $link->query($sqle);
+   while ($row = mysqli_fetch_array($resp) ) {
+      $region = $row['NOMBRE_REGION']; 
+   }
 
-if($result){
-	echo "OK";
+   $array = array( 
+   				"nombre"      => limpiar_signos($_POST['nomapellido']),
+   				"alias"       => limpiar_signos($_POST['alias']),
+                   "rut"  		  => $_POST['rut'],
+                   "email"       => limpiar_signos($_POST['email']),
+                   "region"      => $region,
+                   "comuna"      => $_POST['comunas'],
+                   "candidato"   => $_POST['candidatos'],
+                   "comoseentero" => $comoseentero,
+                   "fecha"        => date("Y-m-d H:i:s"));
+
+   // almacena los datos
+   $sql  = "INSERT INTO votaciones";
+   $sql .= " (`".implode("`, `", array_keys($array))."`)";
+   $sql .= " VALUES ('".implode("', '", $array)."') ";
+   $result = $link->query($sql) or die(mysqli_error($link));
+   // Cerrar la conexión
+   $link->close();
+
+   if($result){
+   	echo "OK";
+   }else{
+   	echo "ERROR";
+   }
+
 }else{
-	echo "ERROR";
+   echo "DUPLICADO";
 }
